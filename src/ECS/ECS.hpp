541c4cc6d3a3567e5a53b1ea0515671df3421453
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-09-03 08:03:44 -0600

    add partial implementation of ECS header file

--- src/ECS/ECS.hpp
+++ src/ECS/ECS.hpp
@@ -1,7 +1,37 @@
 #ifndef ECS_HPP
 #define ECS_HPP
 
+struct IComponent {
+protected:
+    static int next_id;
+};
 
+template<typename TComponent>
+class Component : public IComponent {
+public:
+    static int get_id() {
+        static int id = next_id++;
+        return id;
+    }
+
+};
+
+
+class Entity {
+private:
+    int id;
+
+public:
+    Entity(int id) : id(id) {}
+    int get_id() const;
+};
+
+class Registry {
+private:
+    int num_entities = 0;
+
+
+};
 
 
 
