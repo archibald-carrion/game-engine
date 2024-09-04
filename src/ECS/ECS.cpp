652e1f0b98632f89c23b4e2eef3d941c22fbd489
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-09-04 08:03:58 -0600

    add operators for the entity class and implementation of add, remove and get entity from System class

---
+++ src/ECS/ECS.cpp
@@ -0,0 +1,27 @@
+#include "ECS.hpp"
+#include <algorithm>
+
+int IComponent::next_id = 0;
+
+int Entity::get_id() const {
+    return id;
+}
+
+void System::add_entity_to_system(Entity entity) {
+    entities.push_back(entity);
+}
+
+void System::remove_entity_from_system(Entity entity) {
+    auto it = std::remove_if(entities.begin(), entities.end(), [&entity](Entity other) {
+        return entity == other;
+    });
+    entities.erase(it, entities.end());
+}
+
+std::vector<Entity> System::get_entities() const {
+    return entities;
+}
+
+const Signature& System::get_signature() const {
+    return componentSignature;
+}
--- src/ECS/ECS.hpp
+++ src/ECS/ECS.hpp
@@ -1,9 +1,16 @@
 #ifndef ECS_HPP
 #define ECS_HPP
 
+#include <bitset>
 #include <memory>
 #include <vector>
 #include "../Utils/Pool.hpp"
+
+
+const unsigned int MAX_COMPONENTS = 64;
+
+//Signature
+typedef std::bitset<MAX_COMPONENTS> Signature;
 
 struct IComponent {
 protected:
@@ -28,6 +35,41 @@ private:
 public:
     Entity(int id) : id(id) {}
     int get_id() const;
+
+    bool operator==(const Entity& other) const {
+        return id == other.id;
+    }
+
+    bool operator!=(const Entity& other) const {
+        return id != other.id;
+    }
+
+    bool operator>(const Entity& other) const {
+        return id > other.id;
+    }
+
+    bool operator<(const Entity& other) const {
+        return id < other.id;
+    }
+};
+
+
+class System {
+private:
+    Signature componentSignature;
+    std::vector<Entity> entities; // entities on which this system operates
+
+public:
+    System()= default;
+    ~System()= default;
+
+    void add_entity_to_system(Entity entity);
+    void remove_entity_from_system(Entity entity);
+    std::vector<Entity> get_entities() const;
+    const Signature& get_signature() const;
+
+    template<typename TComponent>
+    void RequireComponent();
 };
 
 class Registry {
@@ -36,6 +78,12 @@ private:
     std::vector<std::shared_ptr<IPool>> componentsPools;
 
 };
+
+template<typename TComponent>
+void System::RequireComponent() {
+    const int component_id = Component<TComponent>::get_id();
+    componentSignature.set(component_id);
+}
 
 
 
