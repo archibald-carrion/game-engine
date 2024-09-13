b65e046ce8a2a19dcf8f8e34affd7b2ddb549616
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-09-13 10:25:49 -0600

    add implementation of the movement system

---
+++ src/systems/MovementSystem.hpp
@@ -0,0 +1,28 @@
+#ifndef MOVEMENTSYSTEM_HPP
+#define MOVEMENTSYSTEM_HPP
+
+#include "../ECS/ECS.hpp"
+#include "../components/RigidBodyComponent.hpp"
+#include "../components/transform_component.hpp"
+
+class MovementSystem : public System {
+public:
+    MovementSystem() {
+        RequireComponent<RigidBodyComponent>();
+        RequireComponent<TransformComponent>();
+    }
+
+    void Update(double dt) {
+        for(auto entity: get_entities()) {
+            const auto& rigid_body = entity.get_component<RigidBodyComponent>();
+            auto& transform = entity.get_component<TransformComponent>();
+
+            transform.position.x += rigid_body.velocity.x * dt;
+            transform.position.y += rigid_body.velocity.y * dt;
+
+        }
+    }
+
+};
+
+#endif // MOVEMENTSYSTEM_HPP
