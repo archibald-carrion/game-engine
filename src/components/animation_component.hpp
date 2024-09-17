004688032a0f219ed3e73f2437594fb0eae2e2f9
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-09-17 08:24:16 -0600

    add implementation of animation component

---
+++ src/components/animation_component.hpp
@@ -0,0 +1,23 @@
+#ifndef ANIMATION_COMPONENT_HPP
+#define ANIMATION_COMPONENT_HPP
+
+// #include <glm/glm.hpp>
+#include <SDL2/SDL.h>
+
+struct AnimationComponent {
+    int num_frames;
+    int current_frame;
+    int frame_speed_rate;
+    bool is_loop;
+    int start_time;
+
+    AnimationComponent(int num_frames, int frame_speed_rate, bool is_loop) {
+        this->num_frames = num_frames;
+        this->frame_speed_rate = frame_speed_rate;
+        this->is_loop = is_loop;
+        this->current_frame = 1;
+        this->start_time = SDL_GetTicks();
+    }
+};
+
+#endif // ANIMATION_COMPONENT_HPP
