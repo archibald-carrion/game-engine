8e65bbb91b476cadea735d49a34b3feec546cf1f
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-09-19 13:15:17 -0600

    add basic implementation of the scene loader header file

---
+++ src/scene_manager/scene_loader.hpp
@@ -0,0 +1,35 @@
+#ifndef SCENE_LOADER_HPP
+#define SCENE_LOADER_HPP
+
+#include <memory>
+#include <string>
+#include <SDL2/SDL.h>
+#include <sol/sol.hpp>
+
+#include "../assets_manager/assets_manager.hpp"
+#include "../controller_manager/controller_manager.hpp"
+#include "../ECS/ECS.hpp"
+
+class SceneLoader {
+private:
+    void load_sprites(SDL_Renderer* renderer, const sol::table& sprites, std::unique_ptr<AssetsManager>& asset_manager);
+
+    void load_key_actions(const sol::table& keys, std::unique_ptr<ControllerManager> controller_manager);
+
+    void load_entities(sol::state& lua, const sol::table& entities, std::unique_ptr<Registry>& registry);
+
+public:
+    SceneLoader();
+    ~SceneLoader();
+    void load_scene(const std::string& scene_path,
+        sol::state& lua, 
+        std::unique_ptr<AssetsManager>& asset_manager, 
+        std::unique_ptr<ControllerManager> controller_manager, 
+        std::unique_ptr<Registry>& registry, 
+        SDL_Renderer* renderer);
+
+};
+
+
+
+#endif // SCENE_LOADER_HPP
