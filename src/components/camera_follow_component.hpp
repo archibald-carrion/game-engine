d9e2b0df94ceffe9f317f6aa69d29de516c69a2f
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-10-09 11:06:56 -0600

    add empty implementation of  the camera follow component

---
+++ src/components/camera_follow_component.hpp
@@ -0,0 +1,17 @@
+#ifndef CAMERA_FOLLOW_COMPONENT_HPP
+#define CAMERA_FOLLOW_COMPONENT_HPP
+
+struct CameraFollowComponent{
+    CameraFollowComponent() {
+    }
+};
+
+
+
+
+
+
+
+
+
+#endif // CAMERA_FOLLOW_COMPONENT_HPP
