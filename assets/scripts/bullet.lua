332d6cb3971b9c80c9b8025ba4495875a97995ab
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-10-18 11:34:21 -0600

    add new shooting mechanic

--- assets/scripts/bullet.lua
+++ assets/scripts/bullet.lua
@@ -12,25 +12,71 @@ is_bullet_shot = false
 function update()
     -- get the mouse position and check if the mouse_left_button is pressed
     if not is_bullet_shot then
-        mouse_x, mouse_y = get_mouse_position()
+        -- mouse_x, mouse_y = get_mouse_position()
         is_shot_button_pressed = is_mouse_button_pressed("mouse_left_button")
     
         if is_shot_button_pressed then
+            -- shoot the bullet in the direction of the rotation
+            rotation = get_rotation(this)
+
+            -- rotation 0 is point to the top of the screen
+
+            bullet_speed = 50
+
+            -- get the velocity of the bullet
+            velocity_x = 0
+            velocity_y = 0
+
+            if rotation == 0 then
+                velocity_y = -bullet_speed
+
+            elseif rotation == 90 then
+                velocity_x = bullet_speed
+            
+            elseif rotation == 180 then
+                velocity_y = bullet_speed
+            
+            elseif rotation == 270 then
+                velocity_x = -bullet_speed
+            end
+
+            -- check for diagonal movement
+            if rotation == 45 then
+                velocity_x = bullet_speed
+                velocity_y = -bullet_speed
+            
+            elseif rotation == 135 then
+
+                velocity_x = bullet_speed
+                velocity_y = bullet_speed
+            
+            elseif rotation == 225 then
+                velocity_x = -bullet_speed
+                velocity_y = bullet_speed
+            
+            elseif rotation == 315 then
+                velocity_x = -bullet_speed
+                velocity_y = -bullet_speed
+            end
+            
+
+
+
             -- print the mouse position
-            print("[LUA] mouse_x: " .. mouse_x .. " mouse_y: " .. mouse_y)
+            -- print("[LUA] mouse_x: " .. mouse_x .. " mouse_y: " .. mouse_y)
 
-            -- Calculate the direction of the bullet
-            local dx = mouse_x - x
-            local dy = mouse_y - y
-            local distance = math.sqrt(dx * dx + dy * dy)
+            -- -- Calculate the direction of the bullet
+            -- local dx = mouse_x - x
+            -- local dy = mouse_y - y
+            -- local distance = math.sqrt(dx * dx + dy * dy)
 
-            -- Normalize the direction vector to get a unit vector
-            local direction_x = dx / distance
-            local direction_y = dy / distance
+            -- -- Normalize the direction vector to get a unit vector
+            -- local direction_x = dx / distance
+            -- local direction_y = dy / distance
 
-            -- Calculate the velocity based on the direction and speed
-            velocity_x = direction_x * bullet_speed
-            velocity_y = direction_y * bullet_speed
+            -- -- Calculate the velocity based on the direction and speed
+            -- velocity_x = direction_x * bullet_speed
+            -- velocity_y = direction_y * bullet_speed
 
             -- Set the velocity
             set_velocity(this, velocity_x, velocity_y)
@@ -59,6 +105,9 @@ function on_collision(other)
             other_y = other_y + 32
 
             set_position(this, other_x, other_y)
+
+            -- set rotation to player rotation
+            set_rotation(this, get_rotation(other))
         end
     end
 
