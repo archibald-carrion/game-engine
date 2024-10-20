86fb8567cb13f14f521d8ec07bb8615151c16322
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-10-20 11:59:00 -0600

    add internal documentation to lua binding

--- src/binding/lua_binding.hpp
+++ src/binding/lua_binding.hpp
@@ -13,24 +13,45 @@
 #include "../components/sound_component.hpp"
 #include "../components/tag_component.hpp"
 
+// Audio related functions
 
-// Sounds thingies functions 
+/**
+ * @brief Play a sound effect.
+ * @param sound_id The ID of the sound effect to play.
+ */
 void play_sound(const std::string& sound_id) {
     Game::get_instance().audio_manager->play_sound_effect(sound_id);
 }
 
+/**
+ * @brief Play a music track.
+ * @param music_id The ID of the music track to play.
+ */
 void play_music(const std::string& music_id) {
     Game::get_instance().audio_manager->play_music(music_id);
 }
 
+/**
+ * @brief Stop all sounds and music.
+ */
 void stop_all_sounds() {
     Game::get_instance().audio_manager->stop_all_sounds();
 }
 
+// General entity related functions
+
+/**
+ * @brief Kill an entity.
+ * @param e The entity to kill.
+ */
 void kill_entity(Entity e) {
     e.kill();
 }
 
+/**
+ * @brief Reset the bullet.
+ * @param e The bullet entity to reset.
+ */
 void reset_bullet(Entity e) {
     // set velocity to 0
     auto& rigid_body = e.get_component<RigidBodyComponent>();
@@ -44,14 +65,25 @@ void reset_bullet(Entity e) {
     // set the bullet location to the player location
     auto& transform = e.get_component<TransformComponent>();
     transform.position = {static_cast<float>(x), static_cast<float>(y)};
-
 }
 
-
+/**
+ * @brief Update the player location.
+ * @param x The x coordinate of the player.
+ * @param y The y coordinate of the player.
+ */
 void update_player_location(int x, int y) {
     Game::get_instance().player_location = {x, y};
 }
 
+/**
+ * @brief Get the tag of an entity.
+ * @param e The entity to get the tag from.
+ * @return The tag of the entity.
+ */
+std::string get_tag(Entity entity){
+    return entity.get_component<TagComponent>().tag;
+}
 
 
 
@@ -97,60 +129,105 @@ void update_player_location(int x, int y) {
 //     sound.sound = Game::get_instance().assets_manager->get_sound(sound_id);
 // }
 
-// Controls
+// Control related functions
 
+/**
+ * @brief Get the mouse position.
+ * @return A tuple containing the x and y coordinates of the mouse.
+ */
 std::tuple<int, int> get_mouse_position() {
     return Game::get_instance().controller_manager->get_mouse_position();
 }
 
+/**
+ * @brief Check if a mouse button is pressed.
+ * @param button The button to check.
+ * @return True if the button is pressed, false otherwise.
+ */
 bool is_mouse_button_pressed(const std::string& button) {
     return Game::get_instance().controller_manager->is_mouse_button_pressed(button);
 }
 
+/**
+ * @brief Check if an action is activated.
+ * @param action The action to check.
+ * @return True if the action is activated, false otherwise.
+ */
 bool is_action_activated(const std::string& action) {
     return Game::get_instance().controller_manager->is_key_pressed(action);
 }
 
 
-// Rigid body component
+// Movement, rotation and position related functions
 
+/**
+ * @brief Set the velocity of an entity.
+ * @param e The entity to set the velocity for.
+ * @param x The x component of the velocity.
+ * @param y The y component of the velocity.
+ */
 void set_velocity(Entity e, float x, float y) {
     auto& rigid_body = e.get_component<RigidBodyComponent>();
     rigid_body.velocity.x = x;
     rigid_body.velocity.y = y;
 }
 
-// Tag component
-std::string get_tag(Entity entity){
-    return entity.get_component<TagComponent>().tag;
-}
-
-// Transform component
+/**
+ * @brief Set the rotation of an entity.
+ * @param e The entity to set the rotation for.
+ * @param angle The angle to set the rotation to.
+ */
 void set_rotation(Entity e, float angle) {
     auto& transform = e.get_component<TransformComponent>();
     transform.rotation = angle;
 }
 
+/**
+ * @brief Get the rotation of an entity.
+ * @param e The entity to get the rotation from.
+ * @return The rotation of the entity.
+ */
 float get_rotation(Entity e) {
     return e.get_component<TransformComponent>().rotation;
 }
 
+/**
+ * @brief Get the position of an entity.
+ * @param e The entity to get the position from.
+ * @return A tuple containing the x and y coordinates of the entity.
+ */
 std::tuple<int, int> get_position(Entity e) {
     const auto& transform = e.get_component<TransformComponent>();
     return {static_cast<int>(transform.position.x), static_cast<int>(transform.position.y)};
 }
 
+/**
+ * @brief Get the velocity of an entity.
+ * @param e The entity to get the velocity from.
+ * @return A tuple containing the x and y components of the velocity.
+ */
 std::tuple<int, int> get_velocity(Entity e) {
     const auto& rigid_body = e.get_component<RigidBodyComponent>();
     return {static_cast<int>(rigid_body.velocity.x), static_cast<int>(rigid_body.velocity.y)};
 }
 
+/**
+ * @brief Set the position of an entity.
+ * @param e The entity to set the position for.
+ * @param x The x coordinate of the position.
+ * @param y The y coordinate of the position.
+ */
 void set_position(Entity e, int x, int y) {
     auto& transform = e.get_component<TransformComponent>();
     transform.position.x = static_cast<float>(x);
     transform.position.y = static_cast<float>(y);
 }
 
+/**
+ * @brief Get the size of an entity.
+ * @param e The entity to get the size from.
+ * @return A tuple containing the width and height of the entity.
+ */
 std::tuple<int, int> get_size(Entity e) {
     const auto& sprite = e.get_component<SpriteComponent>();
     const auto& transform = e.get_component<TransformComponent>();
@@ -161,61 +238,25 @@ std::tuple<int, int> get_size(Entity e) {
     return {width, height};
 }
 
-// Scenes
+// Scene related functions
+
+/**
+ * @brief Go to a scene.
+ * @param scene_name The name of the scene to go to.
+ */
 void go_to_scene(const std::string& scene_name) {
     Game::get_instance().scene_manager->set_next_scene(scene_name);
     Game::get_instance().scene_manager->stop_scene();
 }
 
-// Collisions
-bool left_collision(Entity e, Entity other) {
-    const auto& e_collider = e.get_component<BoxColliderComponent>();
-    const auto& e_transform = e.get_component<TransformComponent>();
-    const auto& other_collider = other.get_component<BoxColliderComponent>();
-    const auto& other_transform = other.get_component<TransformComponent>();
+// Collision related functions
 
-    float e_x = e_transform.previous_position.x;
-    float e_y = e_transform.previous_position.y;
-
-    float e_h = static_cast<float>(e_collider.height);
-
-    float other_x = other_transform.previous_position.x;
-    float other_y = other_transform.previous_position.y;
-
-    float other_h = static_cast<float>(other_collider.height);
-
-    // check if left side of e is colliding with other
-    return (
-        other_y < e_y + e_h &&
-        other_y + other_h > e_y &&
-        other_x < e_x
-    );
-}
-
-bool right_collision(Entity e, Entity other) {
-    const auto& e_collider = e.get_component<BoxColliderComponent>();
-    const auto& e_transform = e.get_component<TransformComponent>();
-    const auto& other_collider = other.get_component<BoxColliderComponent>();
-    const auto& other_transform = other.get_component<TransformComponent>();
-
-    float e_x = e_transform.previous_position.x;
-    float e_y = e_transform.previous_position.y;
-
-    float e_h = static_cast<float>(e_collider.height);
-
-    float other_x = other_transform.previous_position.x;
-    float other_y = other_transform.previous_position.y;
-
-    float other_h = static_cast<float>(other_collider.height);
-
-    // check if left side of e is colliding with other
-    return (
-        other_y < e_y + e_h &&
-        other_y + other_h > e_y &&
-        other_x > e_x
-    );
-}
-
+/**
+ * @brief Check if two entities are colliding from the top.
+ * @param e The first entity.
+ * @param other The second entity.
+ * @return True if the entities are colliding from the top, false otherwise.
+ */
 bool top_collision(Entity e, Entity other) {
     const auto& e_collider = e.get_component<BoxColliderComponent>();
     const auto& e_transform = e.get_component<TransformComponent>();
@@ -240,6 +281,42 @@ bool top_collision(Entity e, Entity other) {
     );
 }
 
+/**
+ * @brief Check if two entities are colliding from the left.
+ * @param e The first entity.
+ * @param other The second entity.
+ * @return True if the entities are colliding from the left, false otherwise.
+ */
+bool left_collision(Entity e, Entity other) {
+    const auto& e_collider = e.get_component<BoxColliderComponent>();
+    const auto& e_transform = e.get_component<TransformComponent>();
+    const auto& other_collider = other.get_component<BoxColliderComponent>();
+    const auto& other_transform = other.get_component<TransformComponent>();
+
+    float e_x = e_transform.previous_position.x;
+    float e_y = e_transform.previous_position.y;
+
+    float e_h = static_cast<float>(e_collider.height);
+
+    float other_x = other_transform.previous_position.x;
+    float other_y = other_transform.previous_position.y;
+
+    float other_h = static_cast<float>(other_collider.height);
+
+    // check if left side of e is colliding with other
+    return (
+        other_y < e_y + e_h &&
+        other_y + other_h > e_y &&
+        other_x < e_x
+    );
+}
+
+/**
+ * @brief Check if two entities are colliding from the bottom.
+ * @param e The first entity.
+ * @param other The second entity.
+ * @return True if the entities are colliding from the bottom, false otherwise.
+ */
 bool bottom_collision(Entity e, Entity other) {
     const auto& e_collider = e.get_component<BoxColliderComponent>();
     const auto& e_transform = e.get_component<TransformComponent>();
@@ -261,6 +338,36 @@ bool bottom_collision(Entity e, Entity other) {
         other_x < e_x + e_w &&
         other_x + other_w > e_x &&
         other_y > e_y
+    );
+}
+
+/**
+ * @brief Check if two entities are colliding from the right.
+ * @param e The first entity.
+ * @param other The second entity.
+ * @return True if the entities are colliding from the right, false otherwise.
+ */
+bool right_collision(Entity e, Entity other) {
+    const auto& e_collider = e.get_component<BoxColliderComponent>();
+    const auto& e_transform = e.get_component<TransformComponent>();
+    const auto& other_collider = other.get_component<BoxColliderComponent>();
+    const auto& other_transform = other.get_component<TransformComponent>();
+
+    float e_x = e_transform.previous_position.x;
+    float e_y = e_transform.previous_position.y;
+
+    float e_h = static_cast<float>(e_collider.height);
+
+    float other_x = other_transform.previous_position.x;
+    float other_y = other_transform.previous_position.y;
+
+    float other_h = static_cast<float>(other_collider.height);
+
+    // check if left side of e is colliding with other
+    return (
+        other_y < e_y + e_h &&
+        other_y + other_h > e_y &&
+        other_x > e_x
     );
 }
 
