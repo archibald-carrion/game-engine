6592286dbacaa3c4628a0c2469d884e1a7e7d857
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-10-20 12:10:00 -0600

    add internal documentation to the the rigid body component

--- src/components/RigidBodyComponent.hpp
+++ src/components/RigidBodyComponent.hpp
@@ -1,16 +1,18 @@
 #ifndef RIGIDBODYCOMPONENT_HPP
 #define RIGIDBODYCOMPONENT_HPP
 
-#include <glm/glm.hpp>
+#include <glm/glm.hpp> // GLM math library, used for vec2
 
 struct RigidBodyComponent {
-    glm::vec2 velocity;
+    glm::vec2 velocity; // velocity of the rigid body
 
+    /**
+     * @brief Construct a new Rigid Body Component object
+     * @param velocity The velocity of the rigid body
+     */
     RigidBodyComponent(glm::vec2 velocity = glm::vec2(0.0, 0.0)) {
         this->velocity = velocity;
     }
 };
-
-
 
 #endif // RIGIDBODYCOMPONENT_HPP
