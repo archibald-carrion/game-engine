3636150fce7f5fc7c04fa317c8104cd66039b5af
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-10-20 12:09:27 -0600

    add internal documentation to the the clickable component

--- src/components/clickable_component.hpp
+++ src/components/clickable_component.hpp
@@ -2,15 +2,16 @@
 #define CLICKABLE_COMPONENT_HPP
 
 struct ClickableComponent {
-    bool is_clicked;
-    bool is_hovered;
+    bool is_clicked; // flag to indicate if the component is clicked
+    bool is_hovered; // flag to indicate if the component is hovered
 
+    /**
+     * @brief Construct a new Clickable Component object
+     */
     ClickableComponent() {
         is_clicked = false;
         is_hovered = false;
     }
 };
-
-
 
 #endif // CLICKABLE_COMPONENT_HPP
