8b45469940831b3304dce19ad872041289f49116
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-10-20 12:10:34 -0600

    add internal documentation to the script component

--- src/components/script_component.hpp
+++ src/components/script_component.hpp
@@ -1,14 +1,21 @@
 #ifndef SCRIPT_COMPONENT_HPP
 #define SCRIPT_COMPONENT_HPP
 
-#include <sol/sol.hpp>
+#include <sol/sol.hpp> // Lua scripting
 
 struct ScriptComponent {
-    sol::function update;
-    sol::function on_collision;
-    sol::function on_click;
-    sol::function on_init;
+    sol::function update; // update function
+    sol::function on_collision; // on collision function
+    sol::function on_click; // on click function
+    sol::function on_init; // on init function
 
+    /**
+     * @brief Construct a new Script Component object
+     * @param on_collision The on collision function
+     * @param update The update function
+     * @param on_click The on click function
+     * @param on_init The on init function
+     */
     ScriptComponent(sol::function  on_collision = sol::lua_nil, sol::function update= sol::lua_nil, sol::function on_click= sol::lua_nil, sol::function on_init= sol::lua_nil) {
         this->on_collision = on_collision;
         this->update = update;
