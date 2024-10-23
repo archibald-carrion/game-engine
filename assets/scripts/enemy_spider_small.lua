c1e2c10fe41b437ed23ba00d4e66b9ec0f030a1e
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2024-10-23 16:45:43 -0600

    fix clipping and problem with right collision

--- assets/scripts/enemy_spider_small.lua
+++ assets/scripts/enemy_spider_small.lua
@@ -11,65 +11,46 @@ function on_collision(other)
     end
 
     if other_tag == "wall_0" or other_tag=="wall_1" then
+
+        this_x, this_y = get_position(this)
+        this_velocity_x, this_velocity_y = get_velocity(this)
+
+        other_x, other_y = get_position(other)
+        other_width, other_height = get_size(other)
+
         if left_collision(this, other) then
-            -- print("[LUA] Left collision")
-            -- avoid superposition
-            this_x, this_y = get_position(this)
-            this_velocity_x, this_velocity_y = get_velocity(this)
 
-            other_x, other_y = get_position(other)
-            other_width, other_height = get_size(other)
-
+            --print("[LUA] Left collision")
             set_position(this, other_x + other_width +1, this_y)
-
             -- react to collision/resolve collision
             set_velocity(this, this_velocity_x * -1, this_velocity_y)
         end
 
         if right_collision(this, other) then
-            -- print("[LUA] Right collision")
-            -- avoid superposition
-            this_x, this_y = get_position(this)
-            this_velocity_x, this_velocity_y = get_velocity(this)
 
-            other_x, other_y = get_position(other)
-            other_width, other_height = get_size(other)
-
-            set_position(this, other_x - other_width -1, this_y)
-
+            print("[LUA] Right collision")
+            set_position(this, this_x-this_velocity_x, this_y)
             -- react to collision/resolve collision
             set_velocity(this, this_velocity_x * -1, this_velocity_y)
 
         end
 
         if top_collision(this, other) then
-            -- print("[LUA] Top collision")
-            -- avoid superposition
-            this_x, this_y = get_position(this)
-            this_velocity_x, this_velocity_y = get_velocity(this)
 
-            other_x, other_y = get_position(other)
-            other_width, other_height = get_size(other)
-
-            set_position(this, this_x, other_y + other_height +1)
-
+           --print("[LUA] Top collision")
+           set_position(this, this_x, other_y + other_height +1)
             -- react to collision/resolve collision
             set_velocity(this, this_velocity_x, this_velocity_y * -1)
+
         end
 
         if bottom_collision(this, other) then
-            -- print("[LUA] Bottom collision")
-            -- avoid superposition
-            this_x, this_y = get_position(this)
-            this_velocity_x, this_velocity_y = get_velocity(this)
-
-            other_x, other_y = get_position(other)
-            other_width, other_height = get_size(other)
-
+            
+            --print("[LUA] Bottom collision")
             set_position(this, this_x, other_y - other_height -1)
-
             -- react to collision/resolve collision
             set_velocity(this, this_velocity_x, this_velocity_y * -1)
+
         end
     end
 
